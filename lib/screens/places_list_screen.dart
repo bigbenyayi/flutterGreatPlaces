81df4cb3df2e1e0d660b433884217350d719ddf5
Benjamin Corben <corbenbenjamin@gmail.com>
2020-01-10 18:17:14 +0100

    Layout beginning

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './add_place_screen.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
@@ -6,7 +7,14 @@ class PlacesListScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Your places"),
-        actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: () {})],
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+          ),
+        ],
       ),
       body: Center(
         child: CircularProgressIndicator(),
