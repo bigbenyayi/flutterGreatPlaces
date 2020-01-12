20aa4a784bab475ecb660ede8eb43a60f2ae295e
Benjamin Corben <corbenbenjamin@gmail.com>
2020-01-12 13:01:18 +0100

    working with SQLlite

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 import '../models/place.dart';
+import '../helper/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -11,16 +12,33 @@ class GreatPlaces with ChangeNotifier {
     return [..._items];
   }
 
-  void addPlace(
-    String title,
-    File image,
-  ) {
+  void addPlace(String title, File image) {
     final newPlace = Place(
         id: DateTime.now().toString(),
         title: title,
         location: null,
         image: image);
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            location: null,
+            image: File(item['image']),
+          ),
+        )
+        .toList();
     notifyListeners();
   }
 }
